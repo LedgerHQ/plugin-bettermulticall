df5f4c15e7a0e516fb7551789cfa229938d8130a
yogh333 <jerome.caporossi@gmail.com>
2023-06-02 11:38:44 +0200

    update with new SDK types

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,32 @@
 #![no_std]
 #![no_main]
 
-use context::Transaction;
+//use context::Transaction;
 use nanos_sdk::bindings::os_lib_end;
 
 nanos_sdk::set_panic!(nanos_sdk::exiting_panic);
 
 use nanos_sdk::plugin::{
-    PluginFeedParams, PluginFinalizeParams, PluginGetUiParams, PluginInitParams,
-    PluginInteractionType, PluginQueryUiParams, PluginResult,
+    PluginInteractionType, 
+    PluginFeedParams
+};
+
+use nanos_sdk::starknet::{
+    Call, 
+    AbstractCall,
+    AbstractCallData
 };
 
 use nanos_sdk::testing;
 
-mod context;
+//mod context;
 use testing::debug_print;
-#[macro_use]
-mod debug;
-mod parser;
+//#[macro_use]
+//mod debug;
+//mod parser;
+use heapless::{ String };
 
-use crate::parser::parse_transaction_v1;
+//use crate::parser::parse_transaction_v1;
 
 #[no_mangle]
 extern "C" fn sample_main(arg0: u32) {
@@ -28,63 +35,33 @@ extern "C" fn sample_main(arg0: u32) {
     let operation: PluginInteractionType = value1.into();
 
     match operation {
-        PluginInteractionType::Check => {
-            testing::debug_print("Check plugin presence\n");
-        }
-        PluginInteractionType::Init => {
-            testing::debug_print("Init plugin context\n");
-            let value2 = unsafe { *args.add(1) as *mut PluginInitParams };
-            let params: &mut PluginInitParams = unsafe { &mut *value2 };
-            let tx_info: &Transaction =
-                unsafe { &*(params.core_params.app_data as *const Transaction) };
-
-            let calls = parse_transaction_v1(tx_info);
-            for call in calls {
-                debug_print("- CALL | ");
-                debug_print(&call.function_name);
-                debug_print(": [ ");
-                for param in call.parameters {
-                    debug_print(&param);
-                    debug_print(", ");
-                }
-                debug_print("]\n");
-            }
-
-            params.core_params.plugin_result = PluginResult::Ok;
-        }
         PluginInteractionType::Feed => {
-            testing::debug_print("Feed plugin\n");
+            testing::debug_print("Check plugin Better MultiCall IN\n");
+
             let value2 = unsafe { *args.add(1) as *mut PluginFeedParams };
             let params: &mut PluginFeedParams = unsafe { &mut *value2 };
 
-            params.core_params.plugin_result = PluginResult::Ok;
-        }
-        PluginInteractionType::Finalize => {
-            testing::debug_print("Finalize plugin\n");
-            let value2 = unsafe { *args.add(1) as *mut PluginFinalizeParams };
-            let params: &mut PluginFinalizeParams = unsafe { &mut *value2 };
+            let call: &Call = unsafe {&*(params.data_in[0] as *const Call)};
 
-            params.core_params.plugin_result = PluginResult::Ok;
-        }
-        PluginInteractionType::QueryUi => {
-            testing::debug_print("QueryUI plugin\n");
-            let value2 = unsafe { *args.add(1) as *mut PluginQueryUiParams };
-            let params: &mut PluginQueryUiParams = unsafe { &mut *value2 };
+            let abstract_call: &mut AbstractCall = unsafe {&mut *(params.data_out[0] as *mut AbstractCall)};
+            let call_to_nref: &mut [u8; 256] = unsafe {&mut *(params.data_out[1] as *mut [u8; 256])};
 
-            params.core_params.plugin_result = PluginResult::Ok;
-        }
-        PluginInteractionType::GetUi => {
-            testing::debug_print("GetUI plugin\n");
-            let value2 = unsafe { *args.add(1) as *mut PluginGetUiParams };
-            let params: &mut PluginGetUiParams = unsafe { &mut *value2 };
+            abstract_call.to.value = call.to.value;
+            abstract_call.method = String::from("transfer");
+            abstract_call.selector.value = call.selector.value;
+            for c in &call.calldata {
+                abstract_call.calldata.push(AbstractCallData::Felt(*c)).unwrap();
+            }
+            call_to_nref[0] = 0xDE;call_to_nref[1] = 0xAD;
+            call_to_nref[2] = 0xBE;call_to_nref[3] = 0xEF;
 
-            params.core_params.plugin_result = PluginResult::Ok;
+            testing::debug_print("Check plugin Better MultiCall OUT \n");
+
         }
         _ => {
             testing::debug_print("Not implemented\n");
         }
     }
-
     unsafe {
         os_lib_end();
     }
