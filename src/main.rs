fcb6be65090c8b412d50e521f49389392dcd18db
yogh333 <jerome.caporossi@gmail.com>
2023-06-07 11:20:10 +0200

    update plugin

--- src/main.rs
+++ src/main.rs
@@ -19,14 +19,6 @@ use nanos_sdk::starknet::{
 
 use nanos_sdk::testing;
 
-//mod context;
-//#[macro_use]
-//mod debug;
-//mod parser;
-use heapless::{ String };
-
-//use crate::parser::parse_transaction_v1;
-
 #[no_mangle]
 extern "C" fn sample_main(arg0: u32) {
     let args: *mut u32 = arg0 as *mut u32;
@@ -40,13 +32,11 @@ extern "C" fn sample_main(arg0: u32) {
             let value2 = unsafe { *args.add(1) as *mut PluginFeedParams };
             let params: &mut PluginFeedParams = unsafe { &mut *value2 };
 
-            let call: &Call = unsafe {&*(params.data_in[0] as *const Call)};
-
-            let abstract_call: &mut AbstractCall = unsafe {&mut *(params.data_out[0] as *mut AbstractCall)};
-            let call_to_nref: &mut [u8; 256] = unsafe {&mut *(params.data_out[1] as *mut [u8; 256])};
+            let call: &Call = unsafe {&*(params.data_in as *const Call)};
+            let abstract_call: &mut AbstractCall = unsafe {&mut *(params.data_out as *mut AbstractCall)};
 
             abstract_call.to.value = call.to.value;
-            abstract_call.method = String::from("transfer");
+            abstract_call.method.clone_from(&call.method);
             abstract_call.selector.value = call.selector.value;
             let mut i = 0;
             while i < call.calldata.len() {
@@ -75,13 +65,12 @@ extern "C" fn sample_main(arg0: u32) {
                             panic!("Invalid data: A 2-prefix felt should be followed by two values.");
                         }
                     },
-                    _ => panic!("Unknown prefix for better-multicall."),
+                    _ => {
+                        testing::debug_print("Unknown prefix\n");
+                        panic!("Unknown prefix for better-multicall.")
+                    },
                 }
             }
-
-            call_to_nref[0] = 0xDE;call_to_nref[1] = 0xAD;
-            call_to_nref[2] = 0xBE;call_to_nref[3] = 0xEF;
-
             testing::debug_print("Check plugin Better MultiCall OUT \n");
 
         }
